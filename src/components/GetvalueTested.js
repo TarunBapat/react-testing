01b45fc4a01acd1b55cd8aa3aa6e4b623e8447a7
TarunBapat <32245027+TarunBapat@users.noreply.github.com>
2021-12-06 22:01:53 +0530

    test cases added

--- src/components/GetvalueTested.js
+++ src/components/GetvalueTested.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const GetvalueTested = () => {
-  return <h1>Test value</h1>;
+  const [changedText, setChangedText] = useState(false);
+
+  const updateText = () => {
+    setChangedText(true);
+  };
+  return (
+    <>
+      <h1>Test value</h1>
+      {!changedText && <p>original text</p>}
+      {changedText && <p>updated text</p>}
+      <button onClick={updateText}>click here</button>
+    </>
+  );
 };
 
 export default GetvalueTested;
--- src/components/GetvalueTested.test.js
+++ src/components/GetvalueTested.test.js
@@ -1,12 +1,36 @@
 import GetvalueTested from "./GetvalueTested";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test("checking value", () => {
-  //Arrange
-  render(<GetvalueTested />);
-  //Act
+describe("GetvalueTested", () => {
+  test("checking value", () => {
+    //Arrange
+    render(<GetvalueTested />);
+    //Act
 
-  //Assert
-  const textValue = screen.getByText("Test value");
-  expect(textValue).toBeInTheDocument;
+    //Assert
+    const textValue = screen.getByText("Test value");
+    expect(textValue).toBeInTheDocument;
+  });
+
+  test("when button is not clicked", () => {
+    //Arrange
+    render(<GetvalueTested />);
+    //Act
+
+    //Assert
+    const initialValue = screen.getByText("original text");
+    expect(initialValue).toBeInTheDocument;
+  });
+
+  test("when button is clicked", () => {
+    //Arrange
+    render(<GetvalueTested />);
+    //Act
+    const ButtonByLabel = screen.getByText("original text");
+    userEvent.click(ButtonByLabel);
+    //Assert
+    const initialValue = screen.getByText("click here");
+    expect(initialValue).toBeInTheDocument;
+  });
 });
